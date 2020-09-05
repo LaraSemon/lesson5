fdaf59b2e08c742aa88638192dd7150b0bce8ed7
Lara <tatasemon@mail.ru>
2020-09-05 18:33:30 +0500

    не понимаю почему не выводит текст(

--- src/lesson_5_hw5/lesson5_HW5.java
+++ src/lesson_5_hw5/lesson5_HW5.java
@@ -2,13 +2,13 @@ package lesson_5_hw5;
 
 
 
-public class Person {
-    private String name;
-    private String position;
-    private String eMail;
-    private String telNummer;
-    private int praise;
-    private int age;
+class Person {
+     String name;
+     String position;
+     String eMail;
+     String telNummer;
+    int praise;
+     int age;
 
 
     public Person(String name, String position, String eMail, String telNummer, int praise, int age) {
@@ -20,22 +20,12 @@ public class Person {
         this.age = age;
     }
 
-    public void info() {
-        System.out.println("Имя фамилия работника: " + name + "; Должность: " + position + ";\n " +
-                " Электронный адрес: " + eMail + "; зарплата: " + praise + "; Возраст: " + age);
-    }
+//    public void info() {
+//        System.out.println("Имя фамилия работника: " + name + "; Должность: " + position + ";\n " +
+//                " Электронный адрес: " + eMail + "; зарплата: " + praise + "; Возраст: " + age);
+//    }
 
 
-    public static void main(String[] args) {
-
-        Person[] persArray = new Person[5]; // Вначале объявляем массив объектов
-
-        persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30); // потом для каждой ячейки массива задаем объект
-        persArray[1] = new Person("Sidorov Petr", "Master", "sidorov@mair.ru", "89135432123", 40000, 41);
-        persArray[2] = new Person("Petrova Elena", "Povar", "petrova@gimail.com", "89543212345", 31000, 52);
-        persArray[3] = new Person("Egorov Pavel", "Ohrannik", "egorov@yandex", "89117654321", 18000, 46);
-        persArray[4] = new Person("Kozlova Svetlana", "Yborschica", "kozlova@gimail.com", "89321236543", 11500, 21);
-    }
 
     public String getName() {
         return name;
@@ -54,6 +44,16 @@ public class Person {
     }
 
 
+    public static void main(String[] args) {
+
+        Person[] persArray = new Person[5]; // Вначале объявляем массив объектов
+
+        persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30); // потом для каждой ячейки массива задаем объект
+        persArray[1] = new Person("Sidorov Petr", "Master", "sidorov@mair.ru", "89135432123", 40000, 41);
+        persArray[2] = new Person("Petrova Elena", "Povar", "petrova@gimail.com", "89543212345", 31000, 52);
+        persArray[3] = new Person("Egorov Pavel", "Ohrannik", "egorov@yandex", "89117654321", 18000, 46);
+        persArray[4] = new Person("Kozlova Svetlana", "Yborschica", "kozlova@gimail.com", "89321236543", 11500, 21);
+    }
 
     public void setAge(int age) {
 
